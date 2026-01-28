000ee4911eb3e52ae2b57c935a31c8336fd82185
Ashwin-deals <ashwin11evr@gmail.com>
2026-01-29 01:36:07 +0530

    Fix: Uploading actual e-commerce files instead of a link

--- ecommerce-project/backend/backend/settings.py
+++ ecommerce-project/backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7c0_#ct0f3-b!59b*dfea^yxkh=7*wy9ozdeky^#w#9qqnf7xg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
