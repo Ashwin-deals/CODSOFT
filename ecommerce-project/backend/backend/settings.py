edf93c576832a12ae76f93f7abd74fa48f9d5773
Ashwin-deals <ashwin11evr@gmail.com>
2026-01-29 14:30:34 +0530

    Added templates

--- ecommerce-project/backend/backend/settings.py
+++ ecommerce-project/backend/backend/settings.py
@@ -47,6 +47,23 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'backend.urls'
 WSGI_APPLICATION = 'backend.wsgi.application'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
